418127e5077d309a295083d2127a9c474bec0fb2
elliothimmelfarb <elliothimmelfarb@gmail.com>
2022-06-04 13:31:27 -0700

    space-age iteration 2

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -1,13 +1,31 @@
-module SpaceAge (Planet(..), ageOn) where
+module SpaceAge (Planet (..), ageOn) where
 
-data Planet = Mercury
-            | Venus
-            | Earth
-            | Mars
-            | Jupiter
-            | Saturn
-            | Uranus
-            | Neptune
+data Planet
+  = Mercury
+  | Venus
+  | Earth
+  | Mars
+  | Jupiter
+  | Saturn
+  | Uranus
+  | Neptune
 
-ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+earthYearsTo :: Planet -> Float
+earthYearsTo planet = case planet of
+  Earth -> 1
+  Mercury -> 0.2408467
+  Venus -> 0.61519726
+  Mars -> 1.8808158
+  Jupiter -> 11.862615
+  Saturn -> 29.447498
+  Uranus -> 84.016846
+  Neptune -> 164.79132
+
+secondsInEarthYear :: Float
+secondsInEarthYear = 60 * 60 * 24 * 365.25
+
+secondsToEarthYear :: Integer -> Float
+secondsToEarthYear = (/ secondsInEarthYear) . fromIntegral
+
+ageOn :: Planet -> Integer -> Float
+ageOn planet seconds = secondsToEarthYear seconds / earthYearsTo planet
